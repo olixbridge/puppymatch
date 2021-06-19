63d3dbb932aa6269b39cc5e86e34d83dca8dbe02
Olivia Zihuai Shi <oliviashi@Olivias-MacBook-Pro.local>
2021-06-18 21:24:00 -0400

    first

---
+++ eg.js
@@ -0,0 +1,29 @@
+
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+var q = url.parse(adr, true);
+
+console.log(q.host); //returns 'localhost:8080'
+console.log(q.pathname); //returns '/default.htm'
+console.log(q.search); //returns '?year=2017&month=february'
+
+var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
+console.log(qdata.month); //returns 'february'
+
+
+
+http.createServer(function (req, res) {
+  fs.readFile('mainpage.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
+
+fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
